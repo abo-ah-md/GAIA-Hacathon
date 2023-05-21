7b7e7dd34ede327876b4c142b7e1248ce4c0ea7c
Abdullah alshebel <abo-ah-md@users.noreply.github.com>
2023-05-22 00:07:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const configuration = new Configuration({organization: "org-WzvEuZ1SLy4VY0l0Esorwpli",apiKey: "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",});
+const configuration = new Configuration({organization: "org-WzvEuZ1SLy4VY0l0Esorwpli",apiKey: "",});
 const openai = new OpenAIApi(configuration);
 
 let GPTassesment= null;
